8d3ad6f1325b545c1ad9292a1b9743f51317ed39
tauqeer180 <tauqeermalik855@gmail.com>
2021-02-08 20:07:30 +0500

    MDB

--- src/App.js
+++ src/App.js
@@ -9,13 +9,15 @@ import CI from "./Components/CI";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./store";
+import MainDashboard from './Components/MainDashboard'
 
 function App() {
   return (
     <div className="container">
       <Provider store={store}> 
         <Router>
-          <Route exact path="/" component={CI} />
+          <Route exact path="/" component={MainDashboard} />
+          <Route exact path="/ci" component={CI} />
           <Route exact path="/kyc" component={KYC} />
           <Route exact path="/kyb" component={KYB} />
           <Route exact path="/sdkyb" component={SDKYB} />
